fcb665e1dc9dd48ba5558eacdbdf6047142048a3
1903Ezel <depomelo5@gmail.com>
2025-10-20 14:39:29 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  // BU SATIR ÇOK ÖNEMLİ: Sitenin doğru yolda yayınlanmasını sağlar.
+  // GitHub deponuzun adıyla aynı olmalı.
+  base: '/mediaapp/', 
+  
   plugins: [
     vue(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
-      manifest: false // manifest'i public/manifest.webmanifest ile yönetiyoruz
-    })
+    VitePWA({ registerType: 'autoUpdate' })
   ],
-  build: {
-    outDir: 'dist'
-  },
-  base: '/'
 })
